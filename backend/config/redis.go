1f10fb6ac070da1fcf27e3bd2ed00b7c00c13517
abhay-8 <abhaynimmagadda@gmail.com>
2024-01-21 23:39:18 +0530

    fixing adding logs

--- backend/config/redis.go
+++ backend/config/redis.go
@@ -2,25 +2,59 @@ package config
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/abhay-8/log-ingestor/backend/database"
-	"github.com/go-redis/redis/v8"
+	"github.com/abhay-8/log-ingestor/backend/models"
+	"github.com/abhay-8/log-ingestor/models"
+	"github.com/redis/go-redis/v9"
 )
 
 var ctx = context.TODO()
 
-func GetFromCache(key string) (string, error) {
+func GetFromCache(key string) []models.Log {
 	data, err := database.RedisClient.Get(ctx, key).Result()
 
 	if err != nil {
 		if err == redis.Nil {
-			return "", fmt.Errorf("key does not exist in cache")
+			return nil
 		}
 		Logger.Warnw("Error retrieving data from cache", "Error:", err)
-		return "", fmt.Errorf("error retrieving data from cache")
+		return nil
 
 	}
 
-	return data, nil
+	logs := []models.Log{}
+	if err := json.Unmarshal([]byte(data), &logs); err != nil {
+		Logger.Warnw("Error unmarshalling data from cache", "Error:", err)
+		return nil
+	}
+
+	return logs
+}
+
+func SetInCache(key string, data []models.Log) error {
+	data, err := json.Marshal(logs)
+	if err != nil {
+		Logger.Warnw("Error marshalling data from cache", "Error", err)
+	}
+
+	if err := database.RedisClient.Set(ctx, key, data, 0).Err(); err != nil {
+		Logger.Warnw("Error setting data in cache", "Error:", err)
+	}
+	return nil
+}
+
+func RemovedFromCache(key string) error {
+	err := database.RedisClient.Del(ctx, key).Err()
+	if err != nil {
+		if err == redis.Nil {
+			return nil
+		}
+
+		Logger.Warnw("Error removing data from cache", "Error:", err)
+		return fmt.Errorf("Error removing data from cache")
+	}
+	return nil
 }
---
+++ backend/controller/log.go
@@ -0,0 +1,72 @@
+package controller
+
+import (
+	"github.com/abhay-8/log-ingestor/backend/config"
+	"github.com/abhay-8/log-ingestor/backend/database"
+	"github.com/abhay-8/log-ingestor/backend/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AddEntry(body models.LogEntrySchema) {
+	var log models.Log
+
+	log.Level = body.Level
+	log.Message = body.Message
+	log.ResourceID = body.ResourceID
+	log.Timestamp = body.Timestamp
+	log.TraceID = body.TraceID
+	log.SpanID = body.SpanID
+	log.Commit = body.Commit
+	log.ParentResourceID = body.MetaData.ParentResourceID
+
+	result := database.DB.Create(&log)
+	if result.Error != nil {
+		config.Logger.Errorw("Error while adding a log", "Error:", result.Error)
+	} else {
+		config.FlushCache()
+	}
+}
+
+func AddLog(c *fiber.Ctx) error {
+	var reqBody models.LogEntrySchema
+	if err := c.BodyParser(&reqBody); err != nil {
+		return &fiber.Error{Code: 400, Message: err.Error()}
+	}
+
+	go AddEntry(reqBody)
+
+	return c.Status(200).JSON(fiber.Map{
+		"status": "success",
+	})
+}
+
+func GetAllLogs(c *fiber.Ctx) error {
+	paginatedDB := utils.Paginator(c)(database.DB)
+	page := c.Query("page", "1")
+
+	logsInCache := config.GetFromCache("all_logs_page_" + page)
+	if logsInCache != nil {
+		return c.Status(200).JSON(fiber.Map{
+			"status": "success",
+			"logs":   logsInCache,
+		})
+	}
+
+	var logs []models.Log
+	if err := paginatedDB.
+		Order("timestamp DESC").
+		Find(&logs).Error; err != nil {
+		return &fiber.Error{Code: 500, Message: "Database Error"}
+	}
+
+	config.SetToCache("all_logs_page_"+page, logs)
+
+	return c.Status(200).JSON(fiber.Map{
+		"status": "success",
+		"logs":   logs,
+	})
+}
+
+func GetSearchLogs(c *fiber.Ctx) error {
+	return nil
+}
